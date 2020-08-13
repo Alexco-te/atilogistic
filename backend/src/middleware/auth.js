442ddf16cf591f03e60dbf986e1dbb0af26965b1
aatiencia <aalexandercocote@gmail.com>
2020-08-13 19:23:34 -0400

    a little changes

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -8,7 +8,7 @@ const auth = (req, res, next) => {
         const verified = jwt.verify(token, process.env.JWT_SECRET);
             if (!verifed)
                 return res
-                .status(401)
+                .status(402)
                 .json({msg: "No verificacion de token"})
     } catch (err){
         res.status(500).json({error: err.message})
